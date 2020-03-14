9867720427c8f5d6088f2a2965320098c75da169
Marco Della Vedova <marcodellav@gmail.com>
2020-03-14 19:54:27 +0000

    don't install all this stuff

--- brew.sh
+++ brew.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Install Brew Packages
-brew install python
+#brew install python
 brew install tree
 
 # Install MacOS Applications
-brew cask install google-chrome
+#brew cask install google-chrome
 brew cask install firefox
 brew cask install sublime-text
 brew cask install virtualbox
@@ -13,7 +13,7 @@ brew cask install sourcetree
 brew cask install spotify
 brew cask install discord
 brew cask install google-backup-and-sync
-brew cask install skype
+#brew cask install skype
 brew cask install gimp
 brew cask install vlc
 brew cask install hyperdock
